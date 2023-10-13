970511f2a8821e82382f0fd2cbce015558ac7dc4
DidasMba <daniel.mbarushi@gmail.com>
2023-10-13 14:46:44 +0300

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution in this file!
+
+
+// Task 1: Declare customerName to be 'bob' in global scope
+var customerName = 'bob';
+
+// Task 2: Write a function that modifies customerName
+function upperCaseCustomerName() {
+  if (customerName) {
+    customerName = customerName.toUpperCase();
+  }
+}
+
+// Task 3: Write a function that declares and assigns a variable in global scope
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// Task 4: Write a function that overwrites the best customer variable
+function overwriteBestCustomer() {
+  bestCustomer = 'still not bob';
+}
+
+// Task 5: Declare a constant in global scope and write a function that tries to reassign it
+const leastFavoriteCustomer = 'John Doe';
+
+function changeLeastFavoriteCustomer() {
+  try {
+    leastFavoriteCustomer = 'Jane Smith';
+  } catch (error) {
+    // Handle the error if needed.
+  }
+}
+
+// Export the functions if needed
+module.exports = {
+  customerName,
+  upperCaseCustomerName,
+  setBestCustomer,
+  overwriteBestCustomer,
+  leastFavoriteCustomer,
+  changeLeastFavoriteCustomer,
+};
+
+
